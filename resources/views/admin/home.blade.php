c83ccd5acee2936fc0141aef6511daa92b093dba
Valdir Barbosa <valdirsb@gmail.com>
2020-07-19 17:21:20 -0300

    novo commit

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,12 +3,67 @@
 @section('title', 'Painel')
 
 @section('content_header')
-    <h1>Teste do header</h1>
+    <h1>Dashboard</h1>
 @endsection
 
 @section('content')
 
+<div class="row">
+    <div class="col-md-3">
+        <div class="small-box bg-secondary">
+            <div class="inner">
+                <h3>999</h3>
+                <p>Cliente On-line</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-fw fa-user"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>999</h3>
+                <p>Clientes Cadastrados</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-fw fa-user-friends"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>999</h3>
+                <p>Produtos Cadastrados</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-fw fa-utensils"></i>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>999</h3>
+                <p>Novos Pedidos</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-fw fa-clipboard-check"></i>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <div class="cart">
+            <div class="card-header">
+                <h3>Produtos mais vendidos</h3>
 
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
 
--- resources/views/site/order.blade.php
+++ resources/views/site/order.blade.php
@@ -8,21 +8,45 @@
 VOLTAR
 @endcomponent
 
-
-
-@if (count($orders)>1)
-    vc tem muitos Pedidos
-@else
-   vc tem 1 ou nenhum pedido     
-@endif
-
 <div class="section-container">
     <header class="cart-header">
         <div  class="div-cart">
-            <i class="fas fa-shopping-cart"></i>
+            <i class="fas fa-clipboard-check"></i>
             <h1>Meus Pedidos</h1>
         </div>
     </header>
+    <section>
+        <h3 style="text-align: center; color:red">Acompanhe seu Pedido</h3> 
+        <p style="text-align: center">Tempo extimado para entrega 40min</p>
+        <div class="card cart">
+            <div>
+                <table>
+                    <tr>
+                        <th><h3>Nº</h3></th>
+                        <th style="text-align: center"><h3>Status</h3></th>
+                        <th style="text-align: center"><h3>Valor</h3></th>
+                    </tr> 
+                </table>
+            </div>
+        </div> 
+        @foreach ($orders as $order)
+            <a href="#">
+                <div class="card">
+                    <div>
+                        <table>
+                            <tr>
+                                <td>{{$order->id}}</td>
+                                <td style="width: 200px ; text-align:center">{{$order->status->nome}}</td>
+                                <td>R$ {{$order->valor}}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div> 
+            </a>
+        @endforeach
+    
+        
+    </section>
 
 </div>
 
