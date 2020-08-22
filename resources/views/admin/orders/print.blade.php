0b5f5210a841872f800848d2447ee98773444d04
Valdir Barbosa <valdirsb@gmail.com>
2020-08-22 12:35:37 -0300

    Correções Print

--- resources/views/admin/orders/print.blade.php
+++ resources/views/admin/orders/print.blade.php
@@ -1,45 +1,44 @@
 
 <pre>
-
-=============================
-PEDIDO NÚMERO: {{$pedido->id}}
-Nome: {{$pedido->user->name}}
-=============================
-=========== PEDIDO ===========
+  ======================================
+  PEDIDO NÚMERO: {{$pedido->id}}
+  Nome: {{$pedido->user->name}}
+  ======================================
+  =============== PEDIDO ===============
 
 @foreach ($pedido->products as $product)
-{{$product->pivot->quantidade}} {{$product->produto}} ({{$product->pivot->obs}})
-Valor total: R$ {{number_format($product->pivot->valor_total, 2, ',', '.')}}
-*********
+  {{$product->pivot->quantidade}} {{$product->produto}} ({{$product->pivot->obs}})
+  Valor total: R$ {{number_format($product->pivot->valor_total, 2, ',', '.')}}
+  *********
 
 @endforeach
-==== ENDEREÇO DE ENTREGA ====
+  ======== ENDEREÇO DE ENTREGA ========
 
-{{$pedido->address->logradouro}}, {{$pedido->address->numero}}
-{{$pedido->address->district->nome}}, CEP: {{$pedido->address->cep}}
+  {{$pedido->address->logradouro}}, {{$pedido->address->numero}}
+  {{$pedido->address->district->nome}}, CEP: {{$pedido->address->cep}}
 
-==== VALOR DO FRETE ====
-R$ {{number_format($pedido->frete, 2, ',', '.')}}
-=============================
-Valor Total:
-R$ {{number_format($pedido->valor, 2, ',', '.')}}
-=============================
+  ======== VALOR DO FRETE ========
+  R$ {{number_format($pedido->frete, 2, ',', '.')}}
+  =====================================
+  Valor Total:
+  R$ {{number_format($pedido->valor, 2, ',', '.')}}
+  =====================================
 @if ($pedido->cartao)
-Trazer maquinhinha de cartão
+  Trazer maquinhinha de cartão
 @endif
--------------------------------
+  -------------------------------------
 @if ($pedido->dinheiro)
-Pagamento em dinheiro
-Troco para: R$ {{number_format($pedido->troco, 2, ',', '.')}}
+  Pagamento em dinheiro
+  Troco para: R$ {{number_format($pedido->troco, 2, ',', '.')}}
 @endif
--------------------------------
-OBS gerais:
-{{$pedido->obs}}
+  -------------------------------------
+  OBS gerais:
+  {{$pedido->obs}}
+
+
+
+
 </pre>
-
-
-
-
 
 <script type="text/javascript">
 
